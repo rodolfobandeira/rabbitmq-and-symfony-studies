10c6761ba14a7d1689110aaacfd2175ff4c2b728
rodolfo <rodolfobandeira@users.noreply.github.com>
2016-06-23 01:15:56 -0400

    Update DefaultController.php

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,7 +4,7 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -13,12 +13,10 @@ class DefaultController extends Controller
      * @Route("/", name="homepage")
      * @return string
      */
-    public function indexAction($name)
+    public function indexAction($name = 'rodolfo')
     {
         $this->get('old_sound_rabbit_mq.hello_rabbitmq_producer')->publish($name);
-
-        return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
-        ));
+ 
+	return new Response('Hello Rabbitmq Executed!');
     }
 }
